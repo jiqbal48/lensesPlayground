7ccabe774591e6452846373a88a77c4db4edb5b0
Jahangir Iqbal <jiqbal@vibrenthealth.com>
2019-04-19 15:59:21 -0400

    learning composition
    also index(0) not index[0]

--- src/index.js
+++ src/index.js
@@ -2,12 +2,33 @@ import * as R from "ramda";
 import * as L from "partial.lenses";
 
 var sampleTitles = {
-  titles: [{ language: "en", text: "Title" }, { language: "sv", text: "Rubrik" }]
+  titles: [
+    { language: "en", text: "Title of some book" },
+    { language: "sv", text: "Rubrik ke de booke" }
+  ]
+};
+
+const divider = () => {
+  console.log("-----------------------------");
 };
 
 // get property using old way, and cool new way
 const titlesOldWay = sampleTitles.titles;
 console.log("old way: ", titlesOldWay);
-
 const titlesUsingLenses = L.get(L.prop("titles"), sampleTitles);
 console.log("optics way: ", titlesUsingLenses);
+divider();
+
+// grab just the english title.
+const oldeEnglishTitle = sampleTitles.titles[0].text;
+console.log("oldeEnglishTitle: ", oldeEnglishTitle);
+const newEnglish = L.get(
+  L.compose(
+    L.prop("titles"),
+    L.index(0),
+    L.prop("text")
+  ),
+  sampleTitles
+);
+console.log("hm.. newEnglish is so hard!: ", newEnglish);
+divider();
